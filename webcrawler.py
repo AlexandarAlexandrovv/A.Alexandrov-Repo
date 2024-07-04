a12dec996165c1b70e8b217710005c58d1b8c1ea
AlexandarAlexandrovv <sashkobtw1@gmail.com>
2024-07-04 09:31:11 +0300

    git commit webcrawler

--- webcrawler.py
+++ webcrawler.py
@@ -1,12 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
-import re
-url = 'https://www.днес.bg/?cat=1'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
 
-pattern = r'<div class="aside-tabs active">([.\n\r\s\S]*)</div>'
-matches = soup.find_all(re.compile(pattern))
+# URL to crawl
+url = 'https://www.dnes.bg/?cat=1'
 
-for match in matches:
-    print(match.text.strip())
+while True:
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find all div elements with class "aside-tabs active"
+    matches = soup.find_all('div', {'class': ['aside-tabs', 'active']})
+
+    for match in matches:
+        print(match.get_text().strip())
+
+    # Follow links to next page (if available)
+    next_url = soup.find('a', {'class': 'next-page'})
+    if next_url:
+        url = next_url['href']
+    else:
+        break
